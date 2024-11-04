fbb10bdd4f881f089057f4f245a96c00f6f481c9
opilane <klettenberg@gmail.com>
2024-11-04 15:23:04 +0200

    Christo Klettenberg

--- src/DayOne.java
+++ src/DayOne.java
@@ -5,8 +5,11 @@ public class DayOne {
 
 
         int a = 2;
-        int b = 2;
+        int b = -1;
 
+        // USE && (and)
+        // USE || (or)
+/*
         if (b > a)
         {
             System.out.println("b is bigger than a");
@@ -15,10 +18,27 @@ public class DayOne {
         {
             System.out.println("a is bigger than b");
         }
-
-        else {
-
+        else
+        {
             System.out.println("a and b are equal");
         }
+
+
+        if (b > a && a>0) {
+            System.out.println("b is bigger than a, and a is positive");
+        }
+        else if (b < a && b>0) {
+            System.out.println("a is bigger than b,and b is positive");
+        }
+        else if (b == a || (a<0 && b<0)) {
+            System.out.println("a and b are both equal or both are negative");
+        }
+        else {
+            System.out.println("No specific conditions were met");
+        }
+
+*/
+
+
     }
 }
