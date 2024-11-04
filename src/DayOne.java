3ee1a4d62984af5f121c84ae8f842bb9bcbead9c
opilane <klettenberg@gmail.com>
2024-11-04 14:45:43 +0200

    Christo Klettenberg

--- src/DayOne.java
+++ src/DayOne.java
@@ -1,7 +1,24 @@
+import javax.swing.*;
 
 public class DayOne {
     public static void main(String[] args) {
 
-            System.out.println("Christo Klettenberg");
+
+        int a = 2;
+        int b = 2;
+
+        if (b > a)
+        {
+            System.out.println("b is bigger than a");
+        }
+        else if  (a > b)
+        {
+            System.out.println("a is bigger than b");
+        }
+
+        else {
+
+            System.out.println("a and b are equal");
+        }
     }
 }
