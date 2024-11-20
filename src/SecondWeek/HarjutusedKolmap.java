c55b42dd06179fbb91aef28da366e5dc7f6f4d06
opilane <klettenberg@gmail.com>
2024-11-20 12:58:17 +0200

    Christo Klettenberg

--- src/SecondWeek/HarjutusedKolmap.java
+++ src/SecondWeek/HarjutusedKolmap.java
@@ -10,6 +10,13 @@ public class HarjutusedKolmap {
         //gradeDescription();
         //monthDays();
         //seasonFinder();
+        //tryCatch();
+        //number2StringAndBack();
+        //validNumberCatch();
+        //reverseString();
+        reverseStringToInteger();
+
+
 
 
     }
@@ -219,4 +226,86 @@ public class HarjutusedKolmap {
         }
     }
 
+    public static void tryCatch() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter a number to divide 100 by: ");
+            int number = scanner.nextInt();
+
+            if (number == 0) {
+                System.out.println("Error: Division by zero is not allowed.");
+            } else {
+                double result = 100.0 / number;
+                System.out.printf("100 divided by %d is %.2f%n", number, result);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: Please enter a valid number.");
+        } finally {
+            scanner.close();
+            System.out.println("Thank you for using the program!");
+        }
+    }
+
+    public static void number2StringAndBack() {
+        int originalNumber = 123;
+        // Convert number to string
+        String numberAsString = String.valueOf(originalNumber);
+        // Alternatively: String numberAsString = Integer.toString(originalNumber);
+
+        System.out.println("Number as String: " + numberAsString);
+
+        // Convert string back to number
+        int convertedBackNumber = Integer.parseInt(numberAsString);
+        // Alternatively: int convertedBackNumber = Integer.valueOf(numberAsString);
+
+        System.out.println("Converted Back to Number: " + convertedBackNumber);
+    }
+    public static void validNumberCatch(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Sisestage number: ");
+        String input = scanner.nextLine();
+
+        try {
+            int number = Integer.parseInt(input);
+            System.out.println("Kehtiv number: " + number);
+        } catch (NumberFormatException e) {
+            System.out.println("Viga: Sisestatud väärtus ei ole kehtiv number!");
+        }
+
+        scanner.close();
+    }
+    public static void reverseString(){
+        String text = "123456";
+        System.out.println(text);
+        int length = text.length();
+        System.out.println(length);
+        for(int i = text.length()-1; i >= 0; i--){
+            System.out.print(text.charAt(i));
+        }
+
+
+    }
+    public static void reverseStringToInteger() {
+
+            String text = "123456";
+
+            // etteantud väärtus keeratakse tagurpidi 123456 -> 654321
+            String reversedText = "";
+            for (int i = text.length() - 1; i >= 0; i--) {
+                reversedText += text.charAt(i);
+
+                // sama tehe, mis ülemine lihtsalt, (+=) tõstab vasaku objekti ka paremale
+                // reversedText = reversedText + text.charAt(i);
+            }
+
+            // tagurpidi väärtus muudetakse numbriliseks (int)
+            int reversedNumber = Integer.parseInt(reversedText);
+
+            // prindib tulemi - tagurpidi + int tüüp (numbriline väärtus)
+            System.out.println(reversedNumber);
+        }
 }
+
